18bd5bda3f98c7fafbbb5c1cc38f51f69c989e7f
Declan Moore <declan.moore39@mail.dcu.ie>
2019-12-03 19:00:37 +0000

    day1

---
+++ AoC_day1.1.py
@@ -0,0 +1,10 @@
+inputFile = open("/Users/i342023/Documents/modules.txt", "r")
+
+sum_fuel = 0
+for i in inputFile:
+	mass = int(i)
+	fuel = (mass/3) - 2
+	total_fuel = fuel
+	sum_fuel = sum_fuel + total_fuel
+
+print(sum_fuel)
---
+++ AoC_day1.2.py
@@ -0,0 +1,14 @@
+inputFile = open("/Users/i342023/Documents/modules.txt", "r")
+
+sum_fuel = 0
+for i in inputFile:
+	mass = int(i)
+	fuel = (mass/3) - 2
+	total_fuel = fuel
+	while (fuel > 0):
+		fuel = (fuel/3) - 2
+		if (fuel > 0):
+			total_fuel = total_fuel + fuel
+	sum_fuel = sum_fuel + total_fuel
+
+print(sum_fuel)
